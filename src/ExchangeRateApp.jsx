1fe17b11ecd5467440c9dd63e4efffd8b7f3e453
Sebastian Moreno <sebastian.moreno.contacto@gmail.com>
2022-06-29 21:08:32 -0500

    Improve format/structure🎨 :art:

---
+++ src/ExchangeRateApp.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const ExchangeRateApp = () => {
+  return (
+    <>
+      <div className='container'>
+        <h1>Exchange Rate App</h1>
+        <hr />
+        <p>Welcome to Exchange Rate App</p>
+      </div>
+      
+    </>
+  )
+}
---
+++ src/README.md
@@ -0,0 +1,4 @@
+# Challenge #1: Exchange Rate
+
+Build a service that delivers the latest exchange rate and historical exchange rates
+for a particular currency pair of your choice.
